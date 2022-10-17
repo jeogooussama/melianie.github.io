c8e482d1909d289f66391b0c83a789739c22ff46
jeogooussama <100424628+jeogooussama@users.noreply.github.com>
2022-10-18 00:00:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// get element form html file
+const fistBbox = document.getElementsByClassName('fist-box');
+const secondBox = document.getElementsByClassName("second-box");
+function otherTxt(){
+    fistBbox[0].style.display = "none"
+    secondBox[0].style.display = "flex"
+}
+
